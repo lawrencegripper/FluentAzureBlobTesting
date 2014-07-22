e2ecc2622b6c6e083917a942de3a41784e973b5a
lawrencegripper <info@grippers.co.uk>
2014-07-22 12:46:01 +0100

    Commit of example solution

---
+++ FluentAzureBlobTesting/FluentAzureBlobTesting/ExampleIntegrationTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FluentAzureBlobTesting
+{
+    [TestClass]
+    public class ExampleIntegrationTests
+    {
+        private static CloudStorageAccount account;
+        private static CloudBlobClient blobClient;
+
+        [ClassInitialize]
+        public static void StartAndCleanStorage(TestContext cont)
+        {
+            account = CloudStorageAccount.DevelopmentStorageAccount;
+            blobClient = account.CreateCloudBlobClient();
+            blobClient.StartEmulator();
+        }
+
+        [ClassCleanup]
+        public static void ShutdownStorage()
+        {
+            blobClient.StopEmulator();
+        }
+
+        [TestInitialize]
+        public void CleanAndRestartStorage()
+        {
+            blobClient.ClearBlobItemsFromEmulator();
+        }
+
+        [TestMethod]
+        public void ExampleTest()
+        {
+            var expectedContainerName = "sample";
+            var expectedBlobName = "test";
+            var expectedBlobData = new byte[1];
+
+            Action ExampleUploadToTest = new Action(() =>
+            {
+                CloudBlobContainer container =
+                    blobClient.GetContainerReference(expectedContainerName);
+
+                container.CreateIfNotExists();
+
+                CloudBlockBlob blob =
+                    container.GetBlockBlobReference(expectedBlobName);
+
+                blob.UploadFromByteArray(expectedBlobData, 0, expectedBlobData.Length);
+            });
+
+            ExampleUploadToTest.Invoke();
+
+            blobClient
+                .AssertContainerExists(expectedContainerName)
+                .AssertBlobExists(expectedBlobName)
+                .AssertBlobMatchesData(expectedBlobData);
+        }
+    }
+}
---
+++ FluentAzureBlobTesting/FluentAzureBlobTesting/FluentBlobExtensions.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAzureBlobTesting
+{
+    /// <summary>
+    /// Extensions to allow easy integration testing of Blob storage
+    /// using the emulator. 
+    /// Requires Azure SDK 2.3
+    /// </summary>
+    public static class FluentBlobAssertions
+    {
+        public static CloudBlobContainer AssertContainerExists(this CloudBlobClient blobClient, string containerName)
+        {
+            var container = blobClient.GetContainerReference(containerName);
+            if (!container.Exists())
+            {
+                Assert.Fail(string.Format("Container doesn't exist '{0}'", containerName));
+            }
+
+            Trace.WriteLine(string.Format("Container exists '{0}'", containerName));
+
+            return container;
+        }
+
+        public static ICloudBlob AssertBlobExists(this CloudBlobContainer blobContainer, string blobName)
+        {
+            var blob = blobContainer.GetBlobReferenceFromServer(blobName);
+            if (!blob.Exists())
+            {
+                Assert.Fail(string.Format("Blog doesn't exist '{0}' in conatiner '{1}'", blobName, blobContainer.Name));
+            }
+
+            Trace.WriteLine(string.Format("Blob exists '{0}'", blobName));
+
+            return blob;
+        }
+
+        public static ICloudBlob AssertBlobMatchesData(this ICloudBlob blob, byte[] expectedData)
+        {
+            byte[] blobData = new byte[blob.Properties.Length];
+            var blobBytes = blob.DownloadToByteArray(blobData, 0);
+
+            if (!blobData.SequenceEqual(expectedData))
+            {
+                Assert.Fail(string.Format("Blob data doesn't match blobName '{0}'", blob.Name));
+            }
+
+            Trace.WriteLine(string.Format("Blob data matches blobName '{0}'", blob.Name));
+
+            return blob;
+        }
+
+        public static CloudBlobClient ClearBlobItemsFromEmulator(this CloudBlobClient blobClient)
+        {
+            ExecuteCommandOnEmulator("clear blob");
+
+            return blobClient;
+        }
+
+        public static CloudBlobClient StartEmulator(this CloudBlobClient blobClient)
+        {
+            ExecuteCommandOnEmulator("start");
+
+            return blobClient;
+        }
+
+        public static CloudBlobClient StopEmulator(this CloudBlobClient blobClient)
+        {
+            ExecuteCommandOnEmulator("stop");
+
+            return blobClient;
+        }
+
+        private static void ExecuteCommandOnEmulator(string arguments)
+        {
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                Arguments = arguments,
+                FileName = StorageEmulatorLocation
+            };
+            Process proc = new Process
+            {
+                StartInfo = start
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static string StorageEmulatorLocation = @"C:\Program Files (x86)\Microsoft SDKs\Windows Azure\Storage Emulator\WAStorageEmulator.exe";
+    }
+
+
+}
